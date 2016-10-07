773bca15bf09b5eeb723ba65b4aeab0ef792dc8c
eVookelj <evukelj@stuy.edu>
2016-10-07 18:55:13 -0400

    Finish req

--- linkedList.c
+++ linkedList.c
@@ -5,10 +5,12 @@ struct node { int i; struct node *next; };
 
 void print_list(struct node *head) {
   struct node* tp = head;
+  printf("LL: ");
   while (tp->next != NULL) {
-    printf("%d\n", tp->i);
-    tp++;
+    printf("%d, ", tp->i);
+    tp=tp->next;
   }
+  printf("%d\n", tp->i);
 }
 
 struct node* insert_front( struct node* head, int x) {
@@ -16,22 +18,28 @@ struct node* insert_front( struct node* head, int x) {
   temp->i = x;
   temp->next = head;
   head = temp;
+  return head;
 }
 
 struct node* free_list( struct node* head) {
   struct node* tp = head;
   while (tp->next != NULL) {
     free(tp);
-    tp++;
+    tp=tp->next;
   }
+  free(tp);
 }
 
 int main() {
-  struct node* head;
-  head = (struct node*)malloc(sizeof(head));
+  struct node* head = NULL;
+  head = (struct node*)malloc(sizeof(struct node));
   head->i = 42;
   head->next = NULL;
-  print_list( insert_front(head, 41) );
-  
+  head = insert_front(head,41);
+  print_list(head);
+  free_list(head);
+
+  printf("\nAnd after freeing the list...\n");
+  print_list(head);
   return 0;
 }
